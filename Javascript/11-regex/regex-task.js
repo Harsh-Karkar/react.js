411dbfd289c064f45034a3a1c5571e6492c31d76
harsh karkar <harshkarkar333@gmail.com>
2024-02-27 17:05:49 +0530

    27-2-2024

--- Javascript/11-regex/regex-task.js
+++ Javascript/11-regex/regex-task.js
@@ -1,4 +1,63 @@
 {
-    const regexp = /^Ka...r\sH...h\s02*\s07+\s2..3$/
-    document.write(regexp.test('Karkar Harsh 02 07 2003'))
+  //   const regexp = /^Ka...r\sH...h\s02*\s07+\s2..3$/;
+  //   document.write(regexp.test("Karkar Harsh 02 07 2003"));
+}
+
+/*----- 1. task-1 -----*/
+
+{
+  // let str = prompt();
+  // let email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  // let ValidEmail = email.test(str);
+  // console.log(ValidEmail);
+}
+{
+  // const regex = /[a-zA-Z0-9_\-\.]+[@][a-z]+[\.][a-z]{2,3}/g;
+  // console.log(regex.test("harsh@gmail.com"));
+}
+
+/*----- 2. task-2 -----*/
+
+{
+  // const regex2 = /(\d{3})\D(\d{3})(\d{2})(\d{2})/g;
+  //     const result3 = regex2.exec('My phone number is: 123 1231212');
+  //     console.log(result3);
+}
+
+/*----- 3. task-3 -----*/
+
+{
+  //   const regex2 = /(\d{2})\D(\d{2})(\d{2})(\d{2})(\d{2})/g;
+  //   const result3 = regex2.exec("My phone number is: 12 31231212");
+  //   console.log(result3);
+}
+
+/*----- 4. task-4 -----*/
+
+{
+  //   const regex2 = /(\d{4})\D(\d{4})(\d{2})/g;
+  //   const result3 = regex2.exec("My phone number is: 1231 231212");
+  //   console.log(result3);
+}
+
+/*----- 5. task-5 -----*/
+
+{
+  //   const regex2 = /(\d{2})(-\d{2})(-\d{2})(-\d{2})(-\d{2})/g;
+  //   const result3 = regex2.exec("My phone number is: 12-12-12-12-12 ");
+  //   console.log(result3);
+}
+/*----- 6. task-6 -----*/
+{
+  //   const regex = /^(\d{3}-){2}\d{2}-\d{2}$/;
+  //   const str = "123-123-12-12";
+  //   const isMatch = regex.test(str);
+  //   console.log(isMatch);
+}
+
+/*----- 7. task-7 -----*/
+{
+  //  const regex2 = /(\d{4})\D(-\d{4})(-\d{2})/g;
+  // const result3 = regex2.exec("My phone number is: 1231 -2312-12");
+  // console.log(result3);
 }
